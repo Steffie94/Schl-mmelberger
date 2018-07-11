16f0de2c77d60d1e6dee2f4c503f67d202380f37
Yosetsu <40422116+Yosetsu@users.noreply.github.com>
2018-07-11 16:41:56 +0200

    Erste Versionen vom Spieler so wie Kamera
    Steuerung so wie Bewegung der Kamera müssen noch verbessert werden.

---
+++ KameraSteuerung.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraSteuerung : MonoBehaviour {
+
+    public GameObject ziel;
+    public Vector3 offset;
+    public bool useOffsetValues;
+    public float rotationsGeschwindigkeit;
+    public GameObject pivot;
+
+    public float max;
+    public float min;
+    public bool invertY;
+    // Use this for initialization
+    void Start ()
+    {
+        //Versteckt den Mauszeiger
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rotationsGeschwindigkeit = 5.0f;
+
+        ziel = GameObject.Find("Spieler");
+        pivot = GameObject.Find("Pivot");
+        pivot.transform.position = ziel.transform.position;
+        pivot.transform.parent = ziel.transform;
+
+        if (!useOffsetValues)
+        {
+           offset = ziel.transform.position - transform.position;
+        }
+    }
+	
+	// Damit es nach Spieler Update ausgeführt wird
+	void LateUpdate ()
+    {
+        //Holt die X Position der Maus und Rotiert das Ziel
+        float horizontal = Input.GetAxis("Mouse X") * rotationsGeschwindigkeit;
+        ziel.transform.Rotate(0, horizontal,0);
+
+        //Y Position der Maus holen und den Pivot drehen
+        float vertical = Input.GetAxis("Mouse Y") * rotationsGeschwindigkeit;
+        //pivot.transform.Rotate(vertical, 0, 0);
+        
+        if(invertY)
+        {
+            pivot.transform.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.transform.Rotate(-vertical, 0, 0);
+        }
+
+        //Limit oben/unten für Kamera
+        if(pivot.transform.rotation.eulerAngles.x > max && pivot.transform.rotation.eulerAngles.x < 180f)
+        {
+            pivot.transform.rotation = Quaternion.Euler(max, 0, 0);
+        }
+
+        if (pivot.transform.rotation.eulerAngles.x > 180 && pivot.transform.rotation.eulerAngles.x < 360f + min)
+        {
+            pivot.transform.rotation = Quaternion.Euler(360f + min, 0, 0);
+        }   
+        //Bewegt die Kamera nach der momentanen Rotation des Ziel und des offsets
+        float desiredYAngle = ziel.transform.eulerAngles.y;
+        float desiredXAngle = pivot.transform.eulerAngles.x;
+
+        Quaternion drehung = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = ziel.transform.position - (drehung * offset);
+
+        //Verhinder das die Kamera durch den Boden geht
+        if(transform.position.y < ziel.transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, ziel.transform.position.y, transform.position.z);
+        }
+        //Schaut immer auf das Ziel
+        transform.LookAt(ziel.transform);
+	}
+}
---
+++ SpielerSteuerung.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpielerSteuerung : MonoBehaviour {
+
+    private Vector3 bewegungsRichtung;
+
+    public float bewegungsGeschwindigkeit;
+    public float sprungKraft;
+    public CharacterController controller;
+    public float gravityScale;
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Holt sich den ChracterController
+        controller = GetComponent<CharacterController>();
+
+        //Setzt die bewegungsGeschwindigkeit des Spielers
+        bewegungsGeschwindigkeit = 10.0f;
+
+        //Setzt die sprungKraft des Spielers
+        sprungKraft = 20.0f;
+
+        gravityScale = 5.0f; 
+
+}
+
+// Update is called once per frame
+void Update ()
+    {
+        //Übergibt jedes Frame einen neuen Vector3 an bewgungsRichtung. Wo bei sich die X und Z Werte ändern bei Bewegung und Y nur bei einem Sprung
+        //bewegungsRichtung = new Vector3(Input.GetAxis("Horizontal") * bewegungsGeschwindigkeit, bewegungsRichtung.y, Input.GetAxis("Vertical") * bewegungsGeschwindigkeit);
+
+        float yStore = bewegungsRichtung.y;
+
+        //Lässt den Spieler in beide Richtungen gleichzeitig laufen transform.forward * Input.GetAxis("Vertical")) + 
+        bewegungsRichtung = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal"));
+
+        //Verhindert das man schneller wird wenn man beide Knöpfe gleichzeitig drückt
+        bewegungsRichtung = bewegungsRichtung.normalized * bewegungsGeschwindigkeit;
+
+        //Setzt den Wert wieder zurück
+        bewegungsRichtung.y = yStore;
+
+        //Überprüft ob der Spieler auf dem Boden ist
+        if (controller.isGrounded)
+        {
+            //Zurück setung des Y Wertes. Sonst komisch wenn man von Kanten fällt wegen der Gravitation
+            bewegungsRichtung.y = 0f;
+
+            //Überprüft ob die Sprung Taste gedrückt wird
+            if (Input.GetButtonDown("Jump"))
+            {
+                // Der Y Wert bei der bewegungsRichtung bekommt die Sprungkraft zugewiesen
+                bewegungsRichtung.y = sprungKraft;
+            }
+
+        }
+
+        // Der Y Wert der bewegungsRichtung wird nochmal durch die Game Physics verändert
+        bewegungsRichtung.y = bewegungsRichtung.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+
+        //Spieler bewegt sich wie es die bewegungsRichtung angibt mit abhängigkeit von der Zeit
+        controller.Move(bewegungsRichtung * Time.deltaTime);
+
+        //Setzt den Wert für den Animator auf true solange der Spiele am Boden ist
+        anim.SetBool("AmBoden", controller.isGrounded);
+        //Erhöht den Speed Wert für den Animator
+        anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical"))+ Mathf.Abs(Input.GetAxis("Horizontal"))));
+    }
+}
