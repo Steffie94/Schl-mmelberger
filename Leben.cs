c551aa893277dd08392e04fda77c059ff7a34851
Yosetsu <40422116+Yosetsu@users.noreply.github.com>
2018-07-12 13:32:13 +0200

    Leben und Schaden
    Spieler besitzt nun Leben und kann Schaden erleiden.

---
+++ Leben.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leben : MonoBehaviour {
+
+    public int maxLeben;
+    public int momLeben;
+
+	// Use this for initialization
+	void Start ()
+    {
+        momLeben = maxLeben;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void schadenSpieler(int schaden)
+    {
+        momLeben -= schaden;
+    }
+
+    public void heileSpieler (int heilung)
+    {
+        momLeben += heilung;
+
+        //Verhindert das man mehr Leben als Maximal haben kann
+        if(momLeben > maxLeben)
+        {
+            momLeben = maxLeben;
+        }
+
+    }
+}
---
+++ Schaden.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schaden : MonoBehaviour {
+
+    public int schadensWert = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Schaut ob der Spieler den Trigger berührt
+        if(other.gameObject.tag == "Player")
+        {
+            //Verletzt den Spieler
+            FindObjectOfType<Leben>().schadenSpieler(schadensWert);
+
+            FindObjectOfType<SpieleManager>().hurtHealth(schadensWert);
+        }
+    }
+}
