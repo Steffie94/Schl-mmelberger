2613c692056defa2e972ed81013779266de5d184
Steffie94 <Stefanie.Schljak@gmx.de>
2018-08-03 17:34:45 +0200

    Add files via upload
    Checkpoint noch nicht ganz fertig

---
+++ Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    public ParticleSystem particles;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            other.GetComponent<ResetObject>().Checkpoint();
+            particles.startColor = Color.green;
+        }
+    }
+}
---
+++ ResetObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetObject : MonoBehaviour {
+
+    Vector3 pos;
+    Quaternion rotat;
+
+	// Use this for initialization
+	void Start () {
+        Checkpoint();
+	}
+    public void ResetingMe()
+    {
+        transform.position = pos;
+        transform.rotation = rotat;
+    }
+
+    public void Checkpoint()
+    {
+        pos = transform.position;
+        rotat = transform.rotation;
+    }
+}
