6dccf720b38980345c08067976ec6e836f0bee0e
Sabrina D <sabrina.dev@hotmail.de>
2018-08-26 12:11:41 +0200

    Add files via upload

---
+++ Spikes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+    // Damit wir das Leben vom Spieler benutzen können
+    private Leben live;
+
+    public int hurt = 1;
+
+    void Start ()
+    {
+            live = GameObject.FindGameObjectWithTag("Player").GetComponent<Leben>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+            if (col.CompareTag("Player"))
+            {
+                Vector3 direction = col.transform.position - transform.position;
+                direction = direction.normalized;
+
+                FindObjectOfType<Leben>().schadenSpieler(hurt, direction);
+
+            }
+    }
+}
