8755536eba0da7fa3b457837ca8d88dec2b32b55
Sabrina D <sabrina.dev@hotmail.de>
2018-09-01 22:12:34 +0200

    Add files via upload

---
+++ BaseSpikes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSpikes : MonoBehaviour {
+    //Sorgt dafür das der Player wenn er auf der Base ist nicht verletzt wird nur wenn er die Spikes trifft.
+    //Indem er zum Kind von der Platform wird.
+    private void OnTriggerEnter(Collider other)
+    {
+        //Player wird ein Kind von der Base.
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    //Wenn der PLayer die Base wieder verlässt ist er kein Kind mehr. 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
---
+++ NewSpikes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSpikes : MonoBehaviour {
+
+    //Schnelligkeit der Spikes festlegen.
+    public float speed = 5f;
+    //Wie hoch die Spikes gehen sollen.
+    public float height = 0.5f;
+    // Nach welchem Zeitabstand die Spikes die Bewegung durch führen.
+    private IEnumerator delayTime;
+
+    // Damit wir das Leben vom Spieler benutzen können
+    Leben live;
+    //Wie viel Leben er verliert, wenn er verletzt wird.
+    public int hurt = 1;
+
+    void Start()
+    {
+        //Zugriff auf Players Leben Script und erhalte Leben.
+        live = GameObject.FindGameObjectWithTag("Player").GetComponent<Leben>();
+    }
+
+    // Die Spikes werden hier animiert sich hoch und runter in einem Loop zu bewegen.
+    void Update () {
+
+        //Die aktuelle Position der Spikes wird hier in einer Variabe gespeichert,
+        //sodass wir diese später wieder verwenden können.
+        Vector3 pos = transform.position;
+        
+        //Berechnet was die neue Position sein wird.
+        float newY = Mathf.Sin(Time.time * speed);
+        
+        //Setzt die Spikes auf die neu berechnete Position von Y.
+        transform.position = new Vector3(pos.x, newY * height, pos.z);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            //Zieht der alten position die neue ab und fügt diese dem neuem Vector hinzu.
+            Vector3 altposition = (transform.position - other.transform.position).normalized;
+            //Benutzt die Funktion SchadenSpieler mit neuen Parametern
+            // Erste Parameter reduziert das Leben bei Verletzung
+            //Zweite Parameter wird der Vector hinzugefügt und die Position y * die höhe genommen in der sie hoch geschleudert werden soll.
+            FindObjectOfType<Leben>().schadenSpieler(hurt,altposition * (transform.position.y*5f) );
+        }
+    }
+}
