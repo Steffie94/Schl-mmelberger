fd1ca26d961d82826bf3e93115a6579751346bb9
Steffie94 <Stefanie.Schljak@gmx.de>
2018-09-19 19:03:44 +0200

    MainMenuScript
    Scprit für laden des Spiels und Spiel beenden

--- MainMenu.cs
+++ MainMenu.cs
@@ -2,44 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
 
 public class MainMenu : MonoBehaviour {
 
-    public Button Start;
-    public Button Load;
-    public Button Quit;
+    public string newGameScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
-    //Name der zu ladenen Scene einfügen in Unity Reinschreiben!
-    public string newGameSceneName;
-
-    //Load Game Menu
-    public GameObject loadGameMenu;
-
-    // Läd neue Scene
     public void NewGame()
     {
-        SceneManager.LoadScene(newGameSceneName);
+        SceneManager.LoadScene(newGameScene);
     }
 
-    // Öffnet den Lade Menü Tab 
-    public void OpenLoadMenuPanel()
+    public void QuitGame()
     {
-        loadGameMenu.SetActive(true);
-    }
-    // Öffnet den Lade Menü Tab 
-    public void CloseLoadMenuPanel()
-    {
-        loadGameMenu.SetActive(false);
-    }
-
-
-    // Schiest das Spiel (Funktionirt nur wenn Compeliert)
-    public void ExitGame()
-    {
-        Debug.Log("Exit Game funktionirt nicht im Editor!");
         Application.Quit();
     }
-
 }
