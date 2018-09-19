26ee1d656d4a7736e9066f4fa75b88b6c39e7bba
Steffie94 <Stefanie.Schljak@gmx.de>
2018-09-19 17:49:11 +0200

    Rotation
    Spinner für die Rotierende Plattform
    Physik für die Stärke des Abstoßes des Spielers

---
+++ PhysikPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysikPlayer : MonoBehaviour {
+
+    public float schieben;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        rb.AddForce(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"))*schieben,ForceMode.Acceleration);
+        
+	}
+}
